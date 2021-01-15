1d459ebe2fa5e9c0e070edd34c7db132e0e2a409
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2021-01-14 20:57:46 -0500

    created 01 ONCE MORE

---
+++ Activities/01.wheres-sausage-dog/js/Animal.js
@@ -0,0 +1,17 @@
+class Animal {
+  constructor(x, y, image) {
+    this.x = x;
+    this.y = y;
+    this.image = animalImg;
+    this.angle = 0
+  }
+
+  update() {
+    this.display();
+  }
+
+  display() {
+    push();
+
+  }
+}
