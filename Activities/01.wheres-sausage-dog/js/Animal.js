667e41d34ae8ffb8ebfe5386b8bb3b3aa083e924
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2021-01-16 17:57:21 -0500

    finished activity 01

--- Activities/01.wheres-sausage-dog/js/Animal.js
+++ Activities/01.wheres-sausage-dog/js/Animal.js
@@ -2,8 +2,9 @@ class Animal {
   constructor(x, y, image) {
     this.x = x;
     this.y = y;
-    this.image = animalImg;
-    this.angle = 0
+    this.image = image;
+
+    this.angle = 0;
   }
 
   update() {
@@ -12,6 +13,21 @@ class Animal {
 
   display() {
     push();
+    imageMode(CENTER);
+    translate(this.x, this.y);
+    rotate(this.angle);
+    image(this.image, 0, 0)
+    pop()
+  }
 
+  overlap(x, y) {
+    if (x > this.x - this.image.width / 2 &&
+      x < this.x + this.image.width / 2 &&
+      y > this.y - this.image.height / 2 &&
+      y < this.y + this.image.height / 2) {
+      return true;
+    } else {
+      return false
+    }
   }
 }
--- Activities/01.wheres-sausage-dog/js/script.js
+++ Activities/01.wheres-sausage-dog/js/script.js
@@ -1,31 +1,70 @@
 "use strict";
-
 /*****************
-
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-
+Activity 01 - Where's sausage dog?
+Sarah Hm
 ******************/
 
+//Declare variables
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
+let sausageDogImage = undefined;
+let sausageDog = undefined;
 // preload()
 // Description of preload
 function preload() {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
 
+  sausageDogImage = loadImage(`assets/images/sausage-dog.png`)
 }
-
 
 // setup()
-// Description of setup
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  createAnimals()
+  createSausageDog()
 }
 
+function createAnimals() {
+  //create animals
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
+}
+
+function createSausageDog() {
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImage);
+}
 
 // draw()
-// Description of draw()
 function draw() {
+  background(255, 255, 0);
+  drawAnimals();
+  drawSausageDog();
+}
 
+function drawAnimals() {
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+}
+
+function drawSausageDog() {
+  sausageDog.update();
+}
+
+function mousePressed() {
+  sausageDog.mousePressed();
 }
