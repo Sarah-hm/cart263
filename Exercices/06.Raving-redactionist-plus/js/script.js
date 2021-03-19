7ccb7b9f5b62cdacdc8675ada0643cde17f08d1b
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2021-03-18 20:08:40 -0400

    E06: added scribble sounds

--- Exercices/06.Raving-redactionist-plus/js/script.js
+++ Exercices/06.Raving-redactionist-plus/js/script.js
@@ -6,12 +6,26 @@ Exercice 06 : Raving redactionist plus
 Sarah Hontoy-Major
 
 ******************/
+let scribbleSound = new Audio(`assets/sounds/zapsplat_office_pen_gel_scribble_color_in_paper_pad_005_40223.mp3`)
+
+// function preload() {
+//   scribbleSound = loadSound()
+// }
+
 $(`.top-secret`).on(`click`, redact);
+
 setInterval(revelation, 500);
 
+
 function redact(event) {
+  playScribbleSound();
   $(this).removeClass(`revealed`);
   $(this).addClass(`redacted`);
+
+}
+
+function playScribbleSound() {
+  scribbleSound.play();
 }
 
 function revelation() {
