fc384cdf3e293b31b398ad23849070c1b3a500d8
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2021-03-25 12:50:53 -0400

    e07: added smol top comment to explain why it's so ugly <3

--- Exercices/07.code-taker-plus/js/script.js
+++ Exercices/07.code-taker-plus/js/script.js
@@ -1,6 +1,10 @@
 /**
 Activity 07: code taker
 Sarah Hontoy-Major
+
+This is a really effy exercice, but it is part of the start of my proposal for project 2.
+I was interested in testing things out in order to create a 'dress up' game so I used
+very basic assets just to get around to making a small prototype. 
 */
 
 "use strict";
