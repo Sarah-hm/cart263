f3766e11bffbb30aa0fc27b74c75e32e8e45661a
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2021-03-30 15:31:56 -0400

    E08: Started Exercice 08

---
+++ Exercices/08.desperately-seeking-sadness-plus/js/Boot.js
@@ -0,0 +1,28 @@
+class Boot extends Phaser.Scene {
+
+  constructor() {
+    super({
+      key: `boot`
+    });
+  }
+
+  preload() {
+    //Load assets here
+    this.load.image(`avatar`, `assets/images/avatar.png`)
+    this.load.image(`thumbs-down`, `assets/images/thumbs-down.png`)
+    this.load.image(`thumbs-up`, `assets/images/thumbs-up.png`)
+
+    this.load.on(`complete`, () => {
+      //Reminder that arrow functions to not change the meaning of <this>, hence why it's used here
+      this.scene.start(`play`);
+    });
+  }
+
+  create() {
+
+  }
+
+  update() {
+
+  }
+}
