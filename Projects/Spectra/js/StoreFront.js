7e45b5cbd4e826f8da643b55bb930dc85d98f920
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2021-05-04 18:27:32 -0400

    p2: created first class of storefront with default canvas and background setting

---
+++ Projects/Spectra/js/StoreFront.js
@@ -0,0 +1,12 @@
+class StoreFront {
+  constructor() {
+
+    this.introImgs = {
+      firstFrame: {
+        img: mallEscalatorsImg
+      }
+    }
+
+
+  }
+}
--- Projects/Spectra/js/script.js
+++ Projects/Spectra/js/script.js
@@ -8,6 +8,20 @@ author, and this description to match your project!
 
 "use strict";
 
+let defaultParameters = {
+  canvasWidth: 800,
+  canvasHeight: 600,
+  bg: {
+    r: 255,
+    g: 0,
+    b: 0
+  }
+}
+
+let currentState = undefined // can be : StoreFront, InsideStore...
+
+// storeFront class variables
+
 
 /**
 Description of preload
@@ -21,7 +35,11 @@ function preload() {
 Description of setup
 */
 function setup() {
+  //create a canvas and set its default background color
+  createCanvas(defaultParameters.canvasWidth, defaultParameters.canvasHeight);
+  background(defaultParameters.bg.r, defaultParameters.bg.g, defaultParameters.bg.b)
 
+  currentState = new StoreFront();
 }
 
 
